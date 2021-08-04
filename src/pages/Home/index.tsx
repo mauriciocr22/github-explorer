aef6e9dce2aaa8c5f99b0c094e5a7c4629d88a71
Maurício de Carvalho <mauriciocr223@gmail.com>
2021-08-04 20:07:28 -0300

    feat: fetching user function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 import SVG from 'react-inlinesvg';
 
+import api from '../../services/api';
+
 import githubExplorerImg from '../../assets/github-explorer.svg';
 import searchIcon from '../../assets/search.svg';
 
 import './styles.scss';
 
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 export function Home() {
+  const [ user, setUser ] = useState("");
+  const history = useHistory();
+
+  function handleChange(event: any) {
+    setUser(event.target.value)
+  }
+
+  async function handleSubmit() {
+    try{
+      const fetchUser = await api.get(`${user}`);
+
+      history.push({
+        pathname: `user/${user}`,
+        state: fetchUser.data,
+      });
+    } catch (error) {
+      alert("Usuário não encontrado");
+    }
+  }
+  
   return (
     <div className="home-page">
       <SVG src={githubExplorerImg} className="logo" />
       <div>
-        <input type="text" placeholder="Pesquise por um usuário do Github" className="profile-input"/>
-        <button type="submit" className="search-button"><SVG src={searchIcon}/></button>
+        <input type="text" placeholder="Pesquise por um usuário do Github" value={user} onChange={handleChange} className="profile-input"/>
+        <button type="submit" className="search-button" onClick={handleSubmit}><SVG src={searchIcon}/></button>
       </div>
     </div>
     
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom"
 import { Home } from "./pages/Home";
+import { User } from "./pages/User";
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/user/:login" exact component={User} />
       </Switch>  
     </BrowserRouter>
   )
